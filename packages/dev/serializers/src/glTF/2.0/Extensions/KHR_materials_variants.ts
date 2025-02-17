c83721d0305e4da80b870fde60d393b05ffb6cfc
anupamdas2012 <das.anupam@gmail.com>
2025-02-17 01:03:37 -0600

    all root node attributes work

--- packages/dev/serializers/src/glTF/2.0/Extensions/KHR_materials_variants.ts
+++ packages/dev/serializers/src/glTF/2.0/Extensions/KHR_materials_variants.ts
@@ -27,6 +27,7 @@ export class KHR_materials_variants implements IGLTFExporterExtensionV2 {
 
     /** an array of material variants */
     private _variants: IKHRMaterialVariants_Variant[] = [];
+    private _variantIndexMap: Map<string, number> = new Map();
 
     private _exporter: GLTFExporter;
 
@@ -40,6 +41,17 @@ export class KHR_materials_variants implements IGLTFExporterExtensionV2 {
         this._wasUsed = true;
     }
 
+    private _findRootNode(node: Node): Nullable<Node> {
+        let current: Nullable<Node> = node;
+        while (current) {
+            if (current.name === "__root__") {
+                return current;
+            }
+            current = current.parent;
+        }
+        return null;
+    }
+
     public postExportNodeAsync(
         context: string,
         node: Nullable<INode>,
@@ -49,55 +61,58 @@ export class KHR_materials_variants implements IGLTFExporterExtensionV2 {
         bufferManager: BufferManager
     ): Promise<Nullable<INode>> {
         return new Promise((resolve) => {
+            const rootNode = this._findRootNode(babylonNode);
             if (!this._wasUsed) {
-                resolve(node);
-                return;
+                if (rootNode) {
+                    // We can now access metadata from rootNode._internalMetadata?.gltf?.[NAME]
+                    const metadata = rootNode._internalMetadata?.gltf?.[NAME];
+                    if (metadata) {
+                        const gltf = this._exporter._glTF;
+                        // Get variant names and populate our internal arrays
+                        const variantNames = Object.keys(metadata.variants);
+                        this._variants = variantNames.map((name, index) => {
+                            this._variantIndexMap.set(name, index);
+                            return { name };
+                        });
+                        // Add to extensionsUsed if not already present
+                        if (!gltf.extensionsUsed) {
+                            gltf.extensionsUsed = [];
+                        }
+                        // add name of extension to property
+                        if (!gltf.extensionsUsed.includes(NAME)) {
+                            gltf.extensionsUsed.push(NAME);
+                        }
+                        // add extensions attribute if it does not exist
+                        if (!gltf.extensions) {
+                            gltf.extensions = {};
+                        }
+                        // add all the variants to the root node
+                        // the data in the gltf should look like this:
+                        // "extensions": {
+                        //     "KHR_materials_variants": {
+                        //       "variants": [
+                        //         {
+                        //           "name": "midnight"
+                        //         },
+                        //         {
+                        //           "name": "beach"
+                        //         },
+                        //         {
+                        //           "name": "street"
+                        //         }
+                        //       ]
+                        //     }
+                        //   },
+                        // const variants: IKHRMaterialVariants_Variants = {
+                        //     variants: this._variants,
+                        // };
+                        gltf.extensions[NAME] = {
+                            variants: this._variants, // Was incorrectly assigning the array directly to the extension
+                        } as IKHRMaterialVariants_Variants; // Using the proper interface
+                        this._wasUsed = true;
+                    }
+                }
             }
-
-            const gltf = this._exporter._glTF;
-
-            // Add to extensionsUsed if not already present
-            if (!gltf.extensionsUsed) {
-                gltf.extensionsUsed = [];
-            }
-
-            // add name of extension to property
-            if (!gltf.extensionsUsed.includes(NAME)) {
-                gltf.extensionsUsed.push(NAME);
-            }
-
-            // add extensions attribute if it does not exist
-            if (!gltf.extensions) {
-                gltf.extensions = {};
-            }
-
-            // add all the variants to the root node
-            // the data in the gltf should look like this:
-            // "extensions": {
-            //     "KHR_materials_variants": {
-            //       "variants": [
-            //         {
-            //           "name": "midnight"
-            //         },
-            //         {
-            //           "name": "beach"
-            //         },
-            //         {
-            //           "name": "street"
-            //         }
-            //       ]
-            //     }
-            //   },
-            const variants: IKHRMaterialVariants_Variants = {
-                variants: this._variants,
-            };
-
-            gltf.extensions[NAME] = variants;
-
-            // gltf.extensions[NAME] = {
-            //     variants: this._variants,
-            // } as IKHRMaterialVariantsRoot;
-
             resolve(node);
         });
     }
